f333fc7e28881a393b8aa93513a3382443633123
- <=>
2019-03-17 19:12:31 +0700

    viluong-addscripts(GroundCheck-HeartUI-gamemaster)

---
+++ Assets/Scripts/HeartUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartUI : MonoBehaviour
+{
+    public Sprite[] Heartsprite;
+
+    public Player player;
+
+    public Image Heart;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.ourHealth > 5)
+            player.ourHealth = 5;
+
+
+        if (player.ourHealth < 0)
+            player.ourHealth = 0;
+
+        Heart.sprite = Heartsprite[player.ourHealth];
+    }
+}
