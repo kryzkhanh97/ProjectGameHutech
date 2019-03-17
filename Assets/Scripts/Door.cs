344b4c57d8ae866fb1b158f230fcf4fe2f4e475c
- <=>
2019-03-17 19:03:20 +0700

    thuong-scripts(camerafollow-door-fallingplat)

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+
+    public int Levelload = 1;
+    public gamemaster gm;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("gamemaster").GetComponent<gamemaster>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            savescore();
+            gm.Inputtext.text = ("Press E to enter");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                savescore();
+                SceneManager.LoadScene(Levelload);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.Inputtext.text = ("");
+        }
+    }
+
+    void savescore()
+    {
+        PlayerPrefs.SetInt("points", gm.points);
+    }
+}
---
+++ Assets/Scripts/fallingPlat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlat : MonoBehaviour
+{
+    public Rigidbody2D r2;
+    public float timedelay = 2;
+    // Use this for initialization
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
+        {
+            StartCoroutine(fall());
+        }
+    }
+
+    IEnumerator fall()
+    {
+        yield return new WaitForSeconds(timedelay);
+        r2.bodyType = RigidbodyType2D.Dynamic;
+        yield return 0;
+    }
+}
