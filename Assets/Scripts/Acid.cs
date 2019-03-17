6eb619a0f37a06f129d774701dcf9be8739f4113
- <=>
2019-03-17 19:17:04 +0700

    quelap-addscripts(Acid-AttackCone-AttackTrigger)

---
+++ Assets/Scripts/Acid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acid : MonoBehaviour {
+
+    public Player player;
+    public int damage = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            player.Damage(damage);
+        }
+    }
+}
