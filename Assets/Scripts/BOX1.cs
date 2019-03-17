4b0cf9e96c418b42d8b5d63a04bfe7602aad4624
NguyenVanHao <=>
2019-03-17 18:19:41 +0700

    VanHao-Resources,Scripts(BOX1,Bullet,bulletmonster)

---
+++ Assets/Scripts/BOX1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOX1 : MonoBehaviour {
+
+    public int Health = 150;
+    public AudioSource audiosrc;
+    public AudioClip box;
+    public Player player;
+    public GameObject deatheffect;
+
+    private void Start()
+    {
+
+        audiosrc = gameObject.GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+            Destroy(Instantiate(deatheffect, this.transform.position, this.transform.rotation) as GameObject, 2);
+        }
+    }
+
+    void Damage(int damage)
+    {
+        audiosrc.PlayOneShot(box, 0.8f);
+        Health -= damage;
+    }
+}
---
+++ Assets/Scripts/bulletmonster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletmonster : MonoBehaviour
+{
+
+    public float lifetime = 3;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.SendMessageUpwards("Damage", 1);
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+            Destroy(gameObject);
+    }
+}
+    
+       
+
